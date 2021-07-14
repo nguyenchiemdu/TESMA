f0c3288006dc3ae5961050ca426f3616e396fd3c
hoangnguyen610 <nvh610@gmail.com>
2021-07-14 15:57:29 +0700

    Add main_screen frame & home_screen

--- lib/views/main_screens/mainscreen.dart
+++ lib/views/main_screens/mainscreen.dart
@@ -1,1 +1,68 @@
 // ở đây mình sẽ viết cái khung của màn hình chính, và thanh điều hướng, .....
+import 'package:flutter/material.dart';
+import './home_screen/home_screen.dart';
+import 'placeholder_widget.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //track the index of our currently selected tab
+  int _currentIndex = 0;
+  //a list of widgets that wewant to render based on the currently selected tab
+  final List<Widget> _children = [
+    // PlaceholderWidget(Colors.white),
+    HomeScreen(),
+    PlaceholderWidget(Colors.deepOrange),
+    PlaceholderWidget(Colors.green),
+    PlaceholderWidget(Colors.red),
+    PlaceholderWidget(Colors.blue)
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
+        title: Text('Trang chủ'),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.pinkAccent,
+        unselectedItemColor: Colors.black87,
+        items: [
+          //Expanded(child: child)
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.house_rounded),
+            title: Text('home'),
+          ),
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.manage_search_rounded),
+            title: Text('search'),
+          ),
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner_rounded),
+            title: Text('qr'),
+          ),
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.doorbell_rounded),
+            title: Text('notification'),
+          ),
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt_rounded),
+            title: Text('list'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
