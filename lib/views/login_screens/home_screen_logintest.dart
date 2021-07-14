fd7f0e9764bb019c5e574d47407212ed55d33def
ngoduythinh250601 <81899659+ngoduythinh250601@users.noreply.github.com>
2021-07-14 21:17:26 +0700

    branch register -- sign up screen

--- lib/views/login_screens/home_screen_logintest.dart
+++ lib/views/login_screens/home_screen_logintest.dart
@@ -8,13 +8,28 @@ class HomePageLoginTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
+      body: Column(children: <Widget>[
+        Center(
+          child: Container(
+            child: Text(
+              "Register",
+              style: TextStyle(
+                fontSize: 40.0,
+              ),
+            ),
+            margin: const EdgeInsets.only(top: 40),
+            height: 48.0,
+          ),
+        ),
         Text('Homepage'),
-        ElevatedButton(
+        Center(
+          child: ElevatedButton(
             onPressed: () {
               context.read<AuthService>().signOut();
             },
-            child: Text("Signout"))
+            child: Text("Signout"),
+          ),
+        ),
       ]),
     );
   }
--- lib/views/login_screens/login_screen.dart
+++ lib/views/login_screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../models/firebase_authen.dart';
 import 'package:provider/provider.dart';
+import 'package:tesma/views/regis_screens/regis_screen.dart';
 
 class LoginForm extends StatelessWidget {
   // This widget is the root of your application.
@@ -12,6 +13,9 @@ class LoginForm extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          Container(
+            padding: EdgeInsets.only(top: 30),
+          ),
           TextField(
             decoration: InputDecoration(labelText: "Email"),
             controller: userController,
@@ -21,22 +25,33 @@ class LoginForm extends StatelessWidget {
             controller: passwordController,
           ),
           ElevatedButton(
-              onPressed: () {
-                context
-                    .read<AuthService>()
-                    .signIn(userController.text, passwordController.text);
-              },
-              style: ButtonStyle(
-                  //backgroundColor: Color.fromRGBO(69, 34, 139, 10),
-                  ),
-              child: Text(
-                "Signin",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16.0,
+            onPressed: () {
+              context
+                  .read<AuthService>()
+                  .signIn(userController.text, passwordController.text);
+            },
+            style: ButtonStyle(
+                //backgroundColor: Color.fromRGBO(69, 34, 139, 10),
                 ),
-              ))
+            child: Text(
+              "Signin",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => MyRegisterScreen()));
+            },
+            child: Text(
+              "Sign up",
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ),
         ],
       ),
     );
