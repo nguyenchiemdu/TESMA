6bd28d1b6b51d2d5c0dacdd5a65977cbf9c89f12
truonganletk <truonganletk@gmail.com>
2021-07-21 00:24:30 +0700

    Add Notification screen

--- lib/views/main_screens/notification_screen/notification_screen.dart
+++ lib/views/main_screens/notification_screen/notification_screen.dart
@@ -106,47 +106,7 @@ class NotificationScreens extends StatelessWidget {
                               ],
                             ),
                           ),
-                          Container(
-                            child: Column(
-                              children: [
-                                Container(
-                                  height: 235.0,
-                                  width: 235.0,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: darkPurpleColor,
-                                  ),
-                                  child: Image.asset(
-                                    'assets/image/paperplane.png',
-                                  ),
-                                ),
-                                Container(
-                                  height: 50,
-                                  child: Text(
-                                    'No notification found',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontFamily: 'SegoeUI',
-                                      color: Colors.white,
-                                      fontSize: 24.0,
-                                      fontWeight: FontWeight.w900,
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  child: Text(
-                                    'You’re all caught up!\nCheck back later for new notifications',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontFamily: 'SegoeUI',
-                                      color: greyColor,
-                                      fontSize: 16.0,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+                          Notifications(),
                         ],
                       ),
                     )
@@ -156,5 +116,135 @@ class NotificationScreens extends StatelessWidget {
             ));
       });
     });
+  }
+}
+
+class Notifications extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            height: 80,
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 15,
+                    spreadRadius: 5,
+                  )
+                ]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    Icons.lock_clock,
+                    size: 40,
+                  ),
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Please update your information!",
+                      style: TextStyle(
+                        fontFamily: 'SegoeUI',
+                        color: blackColor,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Container(
+                      height: 17,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.lock_clock,
+                            size: 10,
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            "15 minutes ago",
+                            style: TextStyle(
+                              fontFamily: 'SegoeUI',
+                              color: blackColor,
+                              fontSize: 10,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NotificationsEmpty extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            height: 235.0,
+            width: 235.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: darkPurpleColor,
+            ),
+            child: Image.asset(
+              'assets/image/paperplane.png',
+            ),
+          ),
+          Container(
+            height: 50,
+            child: Text(
+              'No notification found',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'SegoeUI',
+                color: Colors.white,
+                fontSize: 24.0,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              'You’re all caught up!\nCheck back later for new notifications',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'SegoeUI',
+                color: greyColor,
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
