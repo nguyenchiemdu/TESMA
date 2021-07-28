ab7ccab8868d613ee8e2421b9fe084a90fe064f9
truonganletk <truonganletk@gmail.com>
2021-07-28 15:56:56 +0700

    Class card UI and search by keyword

--- lib/views/main_screens/search_screen/classcard.dart
+++ lib/views/main_screens/search_screen/classcard.dart
@@ -1,10 +1,36 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:tesma/constants/color.dart';
 import 'package:tesma/constants/size_config.dart';
 import 'package:tesma/models/classinf.dart';
 
 Widget classCard(BuildContext context, DocumentSnapshot document) {
-  final _class = ClassInf.fromSnapshot(document);
+  final classinf = ClassInf.fromSnapshot(document);
+
+  List listSubject = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  String scheduleText = "";
+  String feeText;
+  String startdateText;
+  String studentsText;
+
+  // Setup scheduleText ---------------------------
+  for (var i = 0; i < 7; i++) {
+    if (classinf.schedule[i] == true)
+      scheduleText += (scheduleText == "" ? "" : ".") + listSubject[i];
+  }
+  scheduleText += " - " + classinf.time;
+
+  // Setup feeText -----------------------------
+  feeText = classinf.fee + "VND/Month";
+
+  // Setup startdateText -----------------------------
+  startdateText = "Start " + classinf.startdate;
+
+  // Setup studentsText -----------------------------
+  studentsText = " " +
+      classinf.numberofstudents.toString() +
+      "/" +
+      classinf.maxstudents.toString();
 
   return Container(
     child: Column(
@@ -28,10 +54,11 @@ Widget classCard(BuildContext context, DocumentSnapshot document) {
               Container(
                 alignment: Alignment.centerLeft,
                 margin: EdgeInsets.only(
-                    left: 8.5 * SizeConfig.widthMultiplier,
+                    left: 5.5 * SizeConfig.widthMultiplier,
+                    right: 5.5 * SizeConfig.widthMultiplier,
                     top: 2.5 * SizeConfig.widthMultiplier),
                 child: Text(
-                  'Class 12 Physics',
+                  classinf.classname,
                   style: TextStyle(
                     color: Color(0xff181a54),
                     fontSize: 24,
@@ -42,11 +69,40 @@ Widget classCard(BuildContext context, DocumentSnapshot document) {
               ),
               Container(
                 margin: EdgeInsets.only(
-                    left: 8.5 * SizeConfig.widthMultiplier,
-                    top: 1.3 * SizeConfig.heightMultiplier),
+                  left: 5.5 * SizeConfig.widthMultiplier,
+                  right: 5.5 * SizeConfig.widthMultiplier,
+                  bottom: 1 * SizeConfig.widthMultiplier,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      "Created by ",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                        fontFamily: 'SegoeUI',
+                      ),
+                    ),
+                    Text(
+                      classinf.hostID,
+                      style: TextStyle(
+                        color: mediumPink,
+                        fontSize: 12,
+                        fontFamily: 'SegoeUI',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  left: 5.5 * SizeConfig.widthMultiplier,
+                  right: 5.5 * SizeConfig.widthMultiplier,
+                  bottom: 1 * SizeConfig.widthMultiplier,
+                ),
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  'Tues.Thus.Sat - 15:00',
+                  scheduleText,
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 12,
@@ -54,37 +110,61 @@ Widget classCard(BuildContext context, DocumentSnapshot document) {
                   ),
                 ),
               ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                      left: 8.5 * SizeConfig.widthMultiplier,
-                    ),
-                    child: Text(
-                      'Next lesson:',
-                      style: TextStyle(
-                        color: Color(0xffef4874),
-                        fontSize: 8,
-                        fontFamily: 'SegoeUI',
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
+              Container(
+                margin: EdgeInsets.only(
+                  left: 5.5 * SizeConfig.widthMultiplier,
+                  right: 5.5 * SizeConfig.widthMultiplier,
+                  bottom: 1 * SizeConfig.widthMultiplier,
+                ),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  feeText,
+                  style: TextStyle(
+                    color: mediumPink,
+                    fontSize: 12,
+                    fontFamily: 'SegoeUI',
+                    fontWeight: FontWeight.w700,
                   ),
-                  Container(
-                    margin: EdgeInsets.only(
-                        left: 1.84 * SizeConfig.widthMultiplier),
-                    child: Text(
-                      'Sat, 17 Jul, 2021',
-                      style: TextStyle(
-                        color: Color(0xffef4874),
-                        fontSize: 8,
-                        fontFamily: 'SegoeUI',
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: 1.5 * SizeConfig.widthMultiplier,
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 5.5 * SizeConfig.widthMultiplier,
+                        right: 5.5 * SizeConfig.widthMultiplier,
+                      ),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        startdateText,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12,
+                          fontFamily: 'SegoeUI',
+                        ),
+                      ),
+                    ),
+                    Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: [
+                        Icon(Icons.supervised_user_circle),
+                        Text(
+                          studentsText,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 12,
+                            fontFamily: 'SegoeUI',
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
